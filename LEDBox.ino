3be41cbbe97501ea9827e63befb8d22a3607f1c9
Maurizio Monticelli <maurizio.monticelli@staempfli.com>
2014-12-09 13:36:25 +0100

    Tannenbaum

--- LEDBox.ino
+++ LEDBox.ino
@@ -5,11 +5,19 @@
 int i = 0;
 int j = 0;
 
+
+
+const byte IMG_TREE [] PROGMEM = {
+0b00000000, 0b11001110, 0b00001001, 0b10011100, 0b00011011, 0b10111000, 0b00111111, 0b11111111, 0b11111111, 0b11111111, 0b00111111, 0b11111111, 0b00011011, 0b10111000, 0b00001001, 0b10011100, 0b00000000, 0b11001110
+};
+#define IMG_TREE_WIDTH 	 9
+#define IMG_TREE_HEIGHT  16
+
 void setup () {
   // Working with Bicolor displays.
   // Make sure that a bicolor display is set in HT1632.h  
-  HT1632.setCLK(5);
-  HT1632.begin(4, 6, 7);
+  HT1632.setCLK(4);
+  HT1632.begin(5, 6, 7);
 }
 
 void loop () {
@@ -19,20 +27,20 @@ void loop () {
   if (~i & 0b01) { // On frames 1 and 3:
     HT1632.selectChannel(0); // Select the first channel
     // Draw a heart:
-    HT1632.drawImage(IMG_HEART, IMG_HEART_WIDTH,  IMG_HEART_HEIGHT, j - IMG_HEART_WIDTH, 3);
+    HT1632.drawImage(IMG_TREE, IMG_TREE_WIDTH,  IMG_TREE_HEIGHT, j - IMG_TREE_WIDTH, 0);
   }
   
   if (~i & 0b10) { // On frames 2 and 3:
     HT1632.selectChannel(1); // Select the second channel
     // Draw a heart:
-    HT1632.drawImage(IMG_HEART, IMG_HEART_WIDTH,  IMG_HEART_HEIGHT, j - IMG_HEART_WIDTH, 3);
+    HT1632.drawImage(IMG_TREE, IMG_TREE_WIDTH,  IMG_TREE_HEIGHT, j - IMG_TREE_WIDTH, 0);
   }
   
   HT1632.render(); // This sends the data in both channels to the screen.
 
   // Get the next number in the sequence, wrapping from 3 back to 0:
   i = (i + 1) % 3;
-  j = (j + 1) % (OUT_SIZE + IMG_HEART_WIDTH * 2);
+  j = (j + 1) % (OUT_SIZE + IMG_TREE_WIDTH * 2);
   
-  delay(150);  
+  delay(300);  
 }
